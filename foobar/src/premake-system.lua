3389df459f4c75c8792d47b59497e599013869b2
Richard Sim <rsim@devras.com>
2024-09-17 21:26:06 -0700

    Updated a comment

--- foobar/src/premake-system.lua
+++ foobar/src/premake-system.lua
@@ -1,4 +1,4 @@
--- Jarod42/premake-cmake for ejecting to CMake with `premake5 cmake`
+-- Use Jarod42/premake-cmake to add a CMake action (`premake5 cmake`)
 require "tools/premake/cmake"
 
 include "tools/premake/utils.lua"
